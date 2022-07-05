2384d5fc138aaa633bd25613a2847abbeae5f5c7
mtzortzi <maria.ioanna.tzortzi@gmail.com>
2022-07-05 14:15:41 +0300

    Add files via upload

---
+++ GTMcrossval_nwrap_nospatial_newparametrized.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python3
+
+"""
+    Graph Transformer for Graph Regression
+
+    @author: mariannatzortzi
+"""
+
+# import libraries
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.modules.linear import Linear
+
+from torch_geometric.nn import TopKPooling 
+from torch_geometric.nn.norm import PairNorm, LayerNorm 
+from torch_geometric.nn import global_mean_pool as gap, global_max_pool as gmp
+
+from layers.Graph_transformer_layer import TransformerConv
+import NodeFeatureEncoder
+import EdgeFeatureEncoder
+
+class GTModel(nn.Module):
+    def __init__(self, num_layers, hidden_dim, heads, feat_dropout, top_k_pool, norm, beta=True):
+
+        super().__init__()
+
+        dense_neurons = 256
+        self.top_k_every_n = top_k_pool # self.top_k_every_n
+        self.num_layers = num_layers
+        self.norm_name = norm
+        self.feat_dropout = feat_dropout
+        self.norm = None
+
+        self.conv_layers = nn.ModuleList([])
+        self.transf_layers = nn.ModuleList([])
+        self.pooling_layers = nn.ModuleList([])
+        self.norm_layers = nn.ModuleList([])
+
+        if self.norm_name == 'pairnorm':
+            self.norm = PairNorm() #self.pairnorm = PairNorm()
+        elif self.norm_name == 'layernorm':
+            self.norm = LayerNorm(hidden_dim) #self.layernorm = LayerNorm(hidden_dim)
+
+
+        """
+            we don't calculate the embeddings here, but we calculate them using the NodeFeatureEncoder and then pass them into our GNN model directly.
+            The call of NodeFeatureEncoder is happening inside the ... so this is happening per batch of graphs!
+        """
+
+        # Transformation Layer
+        #self.conv1 = TransformerConv(hidden_dim, hidden_dim, heads, dropout=0.5, edge_dim=hidden_dim, norm=self.norm, beta=False)
+        ##self.transf1 = Linear(hidden_dim*heads, hidden_dim) # reduce dimensionality and accelerate processing
+        #self.bn1 = nn.BatchNorm1d(hidden_dim*heads)
+        #self.transf1 = Linear(hidden_dim*heads, hidden_dim)
+
+
+        # maybe the number of input channels in the next layer via num_heads * output_channels - search for this!!!!
+        # Other Layers
+        for i in range(self.num_layers):
+            self.conv_layers.append(TransformerConv(hidden_dim, hidden_dim, heads, dropout=0.4, edge_dim=hidden_dim, norm=self.norm, beta=False))#, self.batch_norm, self.residual))   
+            # self.transf_layers.append(Linear(hidden_dim*heads, hidden_dim))
+            # self.norm_layers.append(nn.LayerNorm(hidden_dim))
+            self.norm_layers.append(nn.BatchNorm1d(hidden_dim*heads)) 
+            self.transf_layers.append(Linear(hidden_dim*heads, hidden_dim))
+            #if i % self.top_k_every_n == 0:
+            #   self.pooling_layers.append(TopKPooling(hidden_dim, ratio=0.5)) # Graph pooling ratio default: 0.5   
+
+        # Linear Layers 
+        self.linear1 = Linear(hidden_dim*2, dense_neurons)
+        self.linear2 = Linear(dense_neurons, int(dense_neurons/2))  
+        # self.linear3 = Linear(int(dense_neurons/2), 2)  
+        self.linear3 = Linear(int(dense_neurons/2), 1)
+
+ 
+    def forward(self, batch_graphs): # h_lap_pos_enc=None
+        out = []
+
+        # x, edge_attr, edge_index, batch_index = batch_graphs.x, batch_graphs.edge_attr, batch_graphs.edge_index, batch_graphs.batch
+        print(f'================x.shape in this new batch of graphs {batch_graphs.x.shape} ')#{batch_graphs.x}==============')
+
+        nodefeature = NodeFeatureEncoder.FeatureEncoder(batch_graphs, emb_dim=64) # emb_dim=hidden_dim=64
+        renewed_data = nodefeature(batch_graphs) # forward pass
+
+        edgefeature = EdgeFeatureEncoder.eFeatureEncoder(renewed_data, emb_dim=64)
+        renewed_data = edgefeature(renewed_data) 
+
+        print(f'x after the Embedding layer {renewed_data.x}')
+
+        x, edge_attr, edge_index, batch_index= renewed_data.x, renewed_data.edge_attr, renewed_data.edge_index, renewed_data.batch
+
+        print(f'shape of x after the calculation and summation of the node embeddings {x.shape}')
+        print(f'edge_attr shape {edge_attr.shape}')
+
+        # Initial Transformation
+        #x = self.conv1(x, edge_index, edge_attr)
+        #x = self.bn1(x)
+        #x = torch.nn.functional.elu(self.transf1(x))
+        ##x = self.bn1(x)
+
+        # Holds the intermediate graph representations
+        global_representation = []
+
+        for i in range(self.num_layers):
+            x = self.conv_layers[i](x, edge_index, edge_attr)
+            x  = self.norm_layers[i](x)
+            x = torch.nn.functional.elu(self.transf_layers[i](x))
+            #x = self.norm_layers[i](x)
+            # Always aggregate last layer
+            #if i % self.top_k_every_n == 0 or i == self.num_layers:
+            #    x, edge_index, edge_attr, batch_index, _, _ = self.pooling_layers[int(i/self.top_k_every_n)](x, edge_index, edge_attr, batch_index) 
+            print(f'shape x: {x.shape}, batch_index shape {batch_index.shape}')
+            # Add current representation
+            global_representation.append(torch.cat([gmp(x, batch_index), gap(x, batch_index)], dim=1))
+
+        x = sum(global_representation)
+
+        print(f'out shape after gap, cat and sum {x.shape}') #[batchsize, hiddendim*lendatalist]
+
+        # Output block
+        x = torch.nn.functional.elu(self.linear1(x))
+        x = F.dropout(x, p=0.4, training=self.training)
+        x = torch.nn.functional.elu(self.linear2(x))
+        x = F.dropout(x, p=0.4, training=self.training)
+        x = self.linear3(x)
+
+        print(f'x shape from GTModel {x.shape}')
+
+        return x
+
+
+    # def loss(self, scores, targets):
+    #     loss = nn.L1Loss(reduction='none')(scores, targets)
+    #     return loss
+
+    def loss(self, scores, targets):
+        loss = nn.L1Loss()(scores, targets)
+        return loss
---
+++ train_crossval_no_wrapper_l1lossmean.py
@@ -0,0 +1,179 @@
+#!/usr/bin/python3
+
+"""
+    This script has the train and evaluate functions for our graph regression.
+
+    @author: mariannatzortzi
+"""
+
+import io
+import os  
+import glob
+from textwrap import indent
+import time
+import argparse
+import datetime
+from matplotlib.pyplot import sca
+import numpy as np
+import pandas as pd
+from pandas.io import json
+from torch_geometric import data
+from tqdm import tqdm
+from pathlib import Path, WindowsPath
+from sklearn.preprocessing import StandardScaler
+
+import torch
+import torch.nn as nn
+import torch_geometric
+from torch_geometric.data import Data
+from torch_geometric.loader import DataLoader
+
+def accuracy(batchscores, targets, pct_close):
+    n_items = len(targets)
+    print(f'n_items is {n_items}')
+    print(f'subtract {(torch.abs(batchscores-targets) < pct_close)}')
+    arr_correct = torch.sum((torch.abs(batchscores-targets) < pct_close), dim=1)
+    print(f'arr_correct is {arr_correct}')
+    n_correct = (arr_correct==2).sum()
+    print(f'give me the number of items and how many correct cases do I have in that group {n_items}, {n_correct}')
+    return 
+
+def train_one_epoch(epoch, model, optimizer, train_pg_graphs, batch_size, pct_close):# device
+    
+    trainloader = DataLoader(train_pg_graphs, batch_size=batch_size)
+    # print('\n How many times do I have to traverse the dataset so as to train graphs equal to window(number of iterations) ?', len(dataset), '\n')
+    epoch_train_loss = 0
+    epoch_train_mae = 0
+    model.train()
+    for step, batch_graphs in enumerate(trainloader):
+        start_time_training = time.time()
+        print('========================')
+        print(f'Step {step+1}:')
+        optimizer.zero_grad() # Reset gradients
+
+        batch_scores = model(batch_graphs) # load each DataBatch object
+        print(f'shape of batch scores is {batch_scores.shape}, {batch_scores}')
+
+        # Calculating the loss and the gradients# targets = batch_graphs.y.view(-1)
+        loss_train = model.loss(batch_scores, batch_graphs.y)
+        print(f'batch_graphs.y is {batch_graphs.y.shape}, {batch_graphs.y}')
+        print(f'loss_train is {loss_train.detach().item()}')
+
+        loss_train.backward()
+        optimizer.step()
+        # Update tracking
+        epoch_train_loss += loss_train.detach().item()
+        # epoch_train_mae += MAE(batch_scores, batch_graphs.y)            
+    epoch_train_loss /= (step+1)
+    # epoch_train_mae /= (i+1)
+
+    print('Epoch: {:04d}'.format(epoch),
+            'loss_train: {:.4f}'.format(epoch_train_loss),
+            # 'loss_train_mae: {:.4f}'.format(epoch_train_mae),
+            #'loss_val: {:.4f}'.format(loss_val.item()),
+            'time: {:.4f}s'.format(time.time() - start_time_training)
+    )
+
+    current_train_state = {
+        'epoch': epoch,
+        'loss_train': epoch_train_loss,
+        # 'loss_train_mae': epoch_train_mae
+    } 
+    return current_train_state, optimizer
+
+def evaluate_per_epoch(epoch, model, eval_pg_graphs, batch_size, pct_close): #device
+
+    evalloader = DataLoader(eval_pg_graphs, batch_size=batch_size)
+    epoch_eval_loss = 0
+    model.eval()
+    with torch.no_grad():
+
+        for step, batch_graphs in enumerate(evalloader):
+            start_time_training = time.time()
+            print('========================')
+            print(f'Step {step+1}:')
+
+            batch_scores = model(batch_graphs) # load each DataBatch object
+            print(f'shape of batch scores is {batch_scores.shape}, {batch_scores}')
+
+            # Calculating the loss and the gradients# targets = batch_graphs.y.view(-1)
+            loss_eval = model.loss(batch_scores, batch_graphs.y)
+            print(f'batch_graphs.y is {batch_graphs.y.shape}, {batch_graphs.y}')
+            epoch_eval_loss += loss_eval.detach().item()
+            
+        epoch_eval_loss /= (step+1)
+
+        print('Epoch: {:04d}'.format(epoch),
+                'loss_eval: {:.4f}'.format(epoch_eval_loss),
+                'time: {:.4f}s'.format(time.time() - start_time_training)
+        )
+
+        current_evaluation_state = {
+            'epoch': epoch,
+            'loss_eval': epoch_eval_loss,
+        } 
+    return current_evaluation_state
+
+
+def inverse_scale(meant, stdt, meanb, stdb, y_scaled):
+    # original_values_t = (y_scaled[:,0] * stdt) + meant
+    original_values_t = (y_scaled * stdt) + meant
+    # original_values_b = (y_scaled[:,1] * stdb) + meanb
+    # original_values_b = (y_scaled * stdb) + meanb
+    # print(f'original_values_t {original_values_t.shape} {original_values_t}')
+    # print(f'original_values_b {original_values_b.shape} {original_values_b}')
+    # original_values = torch.stack((original_values_t, original_values_b), dim=1)
+    return original_values_t
+
+
+def test_per_epoch(meant, stdt, meanb, stdb, epoch, model, eval_pg_graphs, batch_size, pct_close): #device
+
+    evalloader = DataLoader(eval_pg_graphs, batch_size=batch_size)
+    # print('\n How many times do I have to traverse the dataset so as to train graphs equal to window(number of iterations) ?', len(dataset), '\n')
+    epoch_eval_loss = 0
+    test_label_list = []
+    test_pred_list = []
+    model.eval()
+    with torch.no_grad():
+
+        for step, batch_graphs in enumerate(evalloader):
+            start_time_training = time.time()
+            print('========================')
+            print(f'Step {step+1}:')
+
+            batch_scores = model(batch_graphs) # load each DataBatch object
+            print(f'shape of batch scores is {batch_scores.shape}, {batch_scores}')
+
+            # Calculating the loss and the gradients# targets = batch_graphs.y.view(-1)
+            loss_eval = model.loss(batch_scores, batch_graphs.y)
+            print(f'test batch_graphs.y is {batch_graphs.y.shape}, {batch_graphs.y}')
+            epoch_eval_loss += loss_eval.detach().item()
+           
+            #endtoend and outputbytes
+            #labels
+            original_values_label = inverse_scale(meant, stdt, meanb, stdb, batch_graphs.y)
+            original_label = pd.DataFrame(original_values_label)
+            original_label['2'] = batch_graphs.kernelname
+            #preds
+            original_values_pred = inverse_scale(meant, stdt, meanb, stdb, batch_scores)
+            original_pred = pd.DataFrame(original_values_pred)
+            original_pred['2'] = batch_graphs.kernelname
+
+            test_label_list.append(original_label)
+            test_pred_list.append(original_pred)
+            print(f'original_pred {original_pred}, original_label {original_label}')
+
+        epoch_eval_loss /= (step+1)
+
+        print('Epoch: {:04d}'.format(epoch),
+                'loss_test: {:.4f}'.format(epoch_eval_loss),
+                'time: {:.4f}s'.format(time.time() - start_time_training)
+        )
+
+        current_test_state = {
+            'epoch': epoch,
+            'loss_test': epoch_eval_loss,
+            'test_labels' : test_label_list,
+            'test_preds' : test_pred_list
+        } 
+    return current_test_state
