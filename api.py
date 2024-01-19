496ae2be9417fb7cfb797f60e63625f2bd721e2d
Charis Kleitsikas <xarisk@Chariss-MacBook-Pro-2.local>
2024-01-19 14:26:18 +0200

    api for RL Planner

--- api.py
+++ api.py
@@ -15,6 +15,14 @@ from torch_geometric.loader import DataLoader
 from GTMcrossval_nwrap_nospatial_newparametrized import GTModel
 import os
 from collections import defaultdict
+import subprocess
+from RL_Planner.utils import Environment
+from RL_Planner.ddqn_agent import Agent
+from RL_Planner.env import Env
+from RL_Planner_AC.utils import Environment
+from RL_Planner_AC.ac_agent import ACAgent
+from RL_Planner_AC.ac_env import ACEnv
+from time import time
 
 app = Flask(__name__)
 api = Api(app)
@@ -22,7 +30,7 @@ api = Api(app)
 EXEC_TIME_WEIGHT = 0.5
 POWER_WEIGHT = 0.5
 
-print(torch.__version__)
+#print(torch.__version__)
 
 PATH_GPU_standard = "gpu_model_standard/model=GTModel_batch=32_nheads=8_hdim=64_kfold=5_lr=0.0001.pt"
 
@@ -78,7 +86,7 @@ def nx_from_json(task_graph_json):
         # Create the graph
         task_id = int(task["operatorId"])
         task_graph.add_node(task_id)
-        print("task ids in nx_from_json",task_id)
+        #print("task ids in nx_from_json",task_id)
         for child in task["children"]:
             task_graph.add_edge(task_id, int(child))
         
@@ -87,12 +95,25 @@ def nx_from_json(task_graph_json):
             parts = task["constraint"].split('-')
             constraints[task_id] = [parts[0].split('_')[1], parts[1]]
         # Decode the source code in string format
+
+
         code_encoded = task["sourceCode"]
+        process = subprocess.Popen(
+        ["clang++", "-emit-llvm", "-S", "-x", "c++", "-", "-o", "-"],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True
+        )
+        llvm_ir, error = process.communicate(input=code_encoded)
+        llvm_ir = llvm_ir.encode("utf-8")
+        code_encoded = base64.b64encode(llvm_ir).decode()
         code_encoded = str.encode(code_encoded)
         code_encoded = base64.b64decode(code_encoded)
         code_str = code_encoded.decode()
         source_codes[task_id] = (code_str, int(task["inputData"]))
-    print(constraints)
+        #print(code_str)
+    #print(constraints)
     return task_graph, source_codes, constraints
 
 def device_info_from_json(nodes_json):
@@ -150,8 +171,8 @@ def device_info_from_json(nodes_json):
         same_node_devices += list(itertools.combinations(current_node_devices, 2))
 
     same_node_devices = set(same_node_devices)
-    print("Devices_info", devices_info)
-    print("same_node_devices", same_node_devices)
+    #print("Devices_info", devices_info)
+    #print("same_node_devices", same_node_devices)
     return devices_info, num_cpu, num_gpu, same_node_devices
 
 
@@ -179,8 +200,8 @@ def device_topology_info(nodes_link_json, devices_info):
                  actual_links.append((v1,v2))
                  #convert to MB/s
                  rates_dict[(v1,v2)] = values3*0.125
-    print("actual_links: ", actual_links)
-    print("rates_dict: ", rates_dict)
+    #print("actual_links: ", actual_links)
+    #print("rates_dict: ", rates_dict)
 
     ## to do: ftiakse tuples me devices, to rates_dict and to actual_links
     return actual_links, rates_dict
@@ -283,9 +304,9 @@ def create_pyg_list(source_codes):
     for operator in source_codes:
         
         pyg_graph = pyg_from_string(source_codes[operator][0], source_codes[operator][1])
-        print(f"Pyg Graph of operator {operator} : {pyg_graph}")
+        #print(f"Pyg Graph of operator {operator} : {pyg_graph}")
         pyg_list.append(pyg_graph)
-        print("Operator from pyg_list:", operator)
+        #print("Operator from pyg_list:", operator)
     return pyg_list
 
 
@@ -330,7 +351,7 @@ class Scheduling(Resource):
 
         try:
             operator_graph, source_codes, constraints = nx_from_json(task_graph_json)
-            print("Constraints",constraints)
+            #print("Constraints",constraints)
             operator_graph = insert_entry_and_exit_nodes(operator_graph)
         except:
             abort(400, description="Invalid form of request data in the operatorGraph")
@@ -373,24 +394,107 @@ class Scheduling(Resource):
         operators_id_list = list(source_codes.keys())
         
         # Calculate the execution times for each operator on each device
-        operator_time_statistics, operator_average_time_statistics, operator_power_statistics = create_task_performance_statistics(operators_id_list,   predicted_times_cpu_standard_real, predicted_times_gpu_standard_real, devices_info, num_cpu, num_gpu)
+        operator_time_statistics, operator_average_time_statistics, operator_power_statistics, actual_operator_time_statistics, actual_operator_power_statistics = create_task_performance_statistics(operators_id_list,   predicted_times_cpu_standard_real, predicted_times_gpu_standard_real, devices_info, num_cpu, num_gpu)
    
         transfer_times, average_transfer_times, _ = create_task_transfer_times(operator_graph, num_cpu, num_gpu, same_node_devices, actual_links, rates_dict)
         
-        print(f"CCR: {np.mean(list(average_transfer_times.values()))/ np.mean(list(operator_average_time_statistics.values()))}")
-        
-        scheduler = HeftScheduler(operator_graph, operator_time_statistics, operator_average_time_statistics, operator_power_statistics,
-                                  transfer_times, average_transfer_times, list(range(num_cpu + num_gpu)),constraints,devices_info)
-
         
         EXEC_TIME_WEIGHT = float(args.get("time_weight"))
         POWER_WEIGHT = 1 - EXEC_TIME_WEIGHT
-        weights = (EXEC_TIME_WEIGHT, POWER_WEIGHT)
-        print(weights)
+        weights = (EXEC_TIME_WEIGHT, POWER_WEIGHT)        
+        #print(f"CCR: {np.mean(list(average_transfer_times.values()))/ np.mean(list(operator_average_time_statistics.values()))}")
+        '''
+        scheduler = HeftScheduler(operator_graph, operator_time_statistics, operator_average_time_statistics, operator_power_statistics,
+                                  transfer_times, average_transfer_times, list(range(num_cpu + num_gpu)),constraints,devices_info, actual_operator_time_statistics, actual_operator_power_statistics)
         scheduling = scheduler.schedule(weights,constraints,devices_info)
-        makespan = scheduler.aft["n_exit"]
+        makespan = scheduler.compute_scheduling_time_execution(scheduling, flag=False)
         power = total_power_consumption(scheduling, operator_power_statistics)
+        actual_score_heft = scheduler.compute_scheduling_time_execution(scheduling, flag=True) #actual finish time of HEFT placement
+        actual_power_con_heft = total_power_consumption(scheduling, actual_operator_power_statistics)        
+        '''
+        #print("Constraints: ", constraints)
+        #print("Devices info: ", devices_info)
+        
+        
+        #'''
+        score_rl = 0
+        placements_rl = []
+        env = ACEnv(operator_graph, operator_time_statistics, operator_average_time_statistics, operator_power_statistics,
+                transfer_times, average_transfer_times, list(range(num_cpu + num_gpu)),constraints,devices_info, num_cpu, num_gpu, 
+                actual_operator_time_statistics, actual_operator_power_statistics, weights)
+        state_dims = 5 + num_cpu + num_gpu
+        #agent = Agent(lr=0.001, gamma=0.99, epsilon=1.0, state_dims=state_dims, n_actions=len(env.action_space),
+        #      batch_size=64, tau=0.05, chkpt_dir='RL_Planner/models/')
+        agent = ACAgent(critic_lr=3e-3, actor_lr=3e-3, gamma=0.99, state_dims=4+num_cpu + num_gpu+3*(4+num_cpu + num_gpu), n_actions=len(env.action_space),
+                chkpt_dir='RL_Planner_AC/models/', name=f'AC_300_5_network', batch_size=128)
+        
+        
+        agent.load_models()
+        observation, G, task_time_statistics, task_average_time_statistics, transfer_times, average_transfer_times, new_constraints, _  = env.reset()
+        done = False
+        while not done:
+            action = agent.choose_action(observation)
+            new_observation, reward, done = env.step(action)
+            #print(f"Reward: {reward}")
+            observation = new_observation
+        #all_placements = env.check(done)
+        #print("all_placements: ", env.all_placements)
+        if len(constraints) == 0:
+            if len(env.all_placements) > 1:
+                first_k = next(iter(env.all_placements))
+                env.all_placements.pop(first_k)
+            smallest_key = min(env.all_placements.keys())
+            score_rl, power_con_rl = env.makespan(env.all_placements[smallest_key], flag=False) #False for our statistics, True for the actual ones'
+            actual_score_rl, actual_power_con_rl = env.makespan(env.all_placements[smallest_key], flag=True)
+            placements_rl.append(env.all_placements[smallest_key])
+            placement = env.all_placements[smallest_key]
+        else:
+            score_rl, power_con_rl = env.makespan(env.current_placement, flag=False) #False for our statistics, True for the actual ones'
+            actual_score_rl, actual_power_con_rl = env.makespan(env.current_placement, flag=True)
+            placements_rl.append(env.current_placement)
+            placement = env.current_placement         
+        
+        #score_rl, power_con_rl = env.makespan(all_placements[smallest_key], flag=False)
+        #actual_score_rl, actual_power_con_rl = env.makespan(all_placements[smallest_key], flag=True)
+        #placements_rl.append(all_placements[smallest_key])
 
+        response = {"placement": [], "objective": {}}
+        for operator_id, device_id in placement.items():
+            if operator_id not in ["n_entry", "n_exit"]:
+                item = {
+                      "operator_id": str(operator_id),
+                      "device_id": devices_info[device_id]["nes_device_id"],
+                      "node_id": devices_info[device_id]["node_id"],
+                      #"device_type": devices_info[device_id]["device_type"]
+                       }
+
+                response["placement"].append(item)
+
+        response["objective"]["time"] = str(score_rl)
+        response["objective"]["actual_time"] = str(actual_score_rl)
+        response["objective"]["power"] = str(power_con_rl)
+        response["objective"]["actual_power"] = str(actual_power_con_rl)
+        #'''
+        #print(weights)
+        
+        '''
+        print(f"RL Placements: {placements_rl}")
+        print(f"RL makespan: {score_rl}")
+        print(f"RL power consumption: {power_con_rl}")
+        print(f"actual makespan of RL placement: {actual_score_rl}")
+        print(f"actual power consumption of RL placement: {actual_power_con_rl}")
+        print()
+        print("==================================")
+        print()
+        print(f"HEFT Placements: {scheduling}")
+        print(f"HEFT makespan: {makespan}")
+        print(f"HEFT power consumption: {power}")
+        print(f"actual makespan of HEFT placement: {actual_score_heft}")
+        print(f"actual power consumption of HEFT placement: {actual_power_con_heft}")
+        '''
+        
+
+        '''
         response = {"placement": [], "objective": {}}
         for operator_id, device_id in scheduling.items():
             if operator_id not in ["n_entry", "n_exit"]:
@@ -398,14 +502,19 @@ class Scheduling(Resource):
                       "operator_id": str(operator_id),
                       "device_id": devices_info[device_id]["nes_device_id"],
                       "node_id": devices_info[device_id]["node_id"],
-                      "device_type": devices_info[device_id]["device_type"]
+                      #"device_type": devices_info[device_id]["device_type"]
                        }
 
                 response["placement"].append(item)
 
         response["objective"]["time"] = str(makespan)
+        response["objective"]["actual_time"] = str(actual_score_heft)
         response["objective"]["power"] = str(power)
-        flag = feasibility_check(response)
+        response["objective"]["actual_power"] = str(actual_power_con_heft)
+        #flag = feasibility_check(response)
+        '''
+
+        flag = False
         if flag == False:
             ####
             filename = 'response.json'
@@ -421,6 +530,7 @@ class Scheduling(Resource):
                 return response
 
             return send_file(filename,mimetype='application/json')
+        
         else:
             abort(jsonify(error='Incorrect operators constraint assignment based on network topology. Please check your constraints again.'), 400)
 
@@ -433,5 +543,4 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--port", default=8081, type=int, help="Port to listen on")
     args = parser.parse_args()
     port = args.port
-
-    app.run(host="0.0.0.0", port=8081, debug=True)
+    app.run(host="0.0.0.0", port=8085, debug=True)
